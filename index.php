8ac36e66a0ad47ec78696276e6de491087f74a68
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-15 11:31:09 -0500

    abstraer navbar v1.0

--- index.php
+++ index.php
@@ -125,26 +125,8 @@ session_start();
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Alia2.0</span>
-                <img style="width: 142px;height: 50px;border-radius: initial !important;" class="img-profile rounded-circle" src="https://aliadostravel.com/wp-content/uploads/2020/12/Aliados.png">
-              </a>
-              <!-- Dropdown - User Information -->
+            <?php include 'navbar.php' ?>
 
-              <div class="dropdown-menu" aria-labelledby="userDropdown">
-                <?php
-                if ($_SESSION['logged']) {
-                ?>
-                  <a class="dropdown-item" href="carga">Carga</a>
-                  <a class="dropdown-item" onclick="alert('En desarrollo')" href="#perfil.php">Perfil</a>
-                  <a class="dropdown-item" href="login/logout.php">Salir</a>
-                <?php } else { ?>
-                  <a class="dropdown-item" href="login/login.php">Iniciar sesión</a>
-                <?php } ?>
-                
-              </div>
-            </li>
 
           </ul>
 
