189b3d3f8462e247c875ae7541c7a20777c9694d
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-15 11:36:04 -0500

    correccion

--- tables.php
+++ tables.php
@@ -255,13 +255,13 @@ switch ($mes) {
 
 
                               <!-- Topbar Navbar -->
-                              <?php include 'navbar.php' ?>
-
-                              <!-- <ul class="navbar-nav ml-auto"> -->
-
-                                    <!-- <div class="topbar-divider d-none d-sm-block"></div> -->
-
+                              
+                              <ul class="navbar-nav ml-auto">
+                                    
+                                    <div class="topbar-divider d-none d-sm-block"></div>
+                                    
                                     <!-- Nav Item - User Information -->
+                                    <?php include 'navbar.php' ?>
                                     <!-- <li class="nav-item dropdown no-arrow"> -->
                                           <!-- <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
                                                 <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">Control Bloqueos V1.0</span> -->
@@ -272,7 +272,7 @@ switch ($mes) {
                                     <!-- </li> -->
 <!--  -->
 <!--  -->
-                              <!-- </ul> -->
+                              </ul>
 
                         </nav>
                         <!-- End of Topbar -->
