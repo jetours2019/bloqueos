c40d1d1176ffdac117d07f51a7a57222a94b53cc
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 10:02:33 -0500

    correciones destinos

--- pages/visualizacion/destinos.php
+++ pages/visualizacion/destinos.php
@@ -66,22 +66,7 @@ switch ($mes) {
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Aliados Travel | Control Charter</title>
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this page -->
+  <?php include '../assets/templates/header.php'; ?>
 
 </head>
 
@@ -90,7 +75,7 @@ switch ($mes) {
   <!-- Page Wrapper -->
   <div id="wrapper">
 
-    <?php include 'aside.php'; ?>
+    <?php include '../assets/templates/aside.php'; ?>
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
@@ -102,7 +87,7 @@ switch ($mes) {
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
 
-          <?php include 'alertas.php' ?>
+          <?php include '../assets/templates/alertas.php' ?>
 
 
           <!-- Topbar Navbar -->
