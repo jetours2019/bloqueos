7bbf2855e2e8d68991e313460592f16858ca02dc
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 12:48:25 -0500

    validaciones extra en tabla

--- pages/visualizacion/tables.php
+++ pages/visualizacion/tables.php
@@ -226,7 +226,7 @@ switch ($mes) {
                                                       $mes22 = $registro['mes2'];
                                                       $dia2 = $registro['dia2'];
                                                       $programa = $registro['programa'];
-                                                      $contentPrograma = (isset($programa) && $programa != null && $programa != "" && $programa != "NO DISPO") ? "<a href='../carga/files/$programa.pdf' title='$programa'>$programa <i class='fas fa-search-plus'></i></a>" : "No disponible";
+                                                      $contentPrograma = (isset($programa) && $programa != null && $programa != "" && $programa != "NO DISPO" && $programa != "0" && $programa != " - " && $programa != "EXCURSIONES") ? "<a href='../carga/files/$programa.pdf' title='$programa'>$programa <i class='fas fa-search-plus'></i></a>" : "No disponible";
                                                       #asignamos el nombre del mes de regreso
                                                       switch ($mes) {
                                                             case 1:
