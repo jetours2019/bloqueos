e5153baba0451eae8f4b0eb06e44ad48dd5029b7
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 12:33:55 -0500

    datatable lenguage

--- assets/templates/datatables.php
+++ assets/templates/datatables.php
@@ -4,6 +4,22 @@
 <script>
     // Call the dataTables jQuery plugin
     $(document).ready(function() {
-        $('#dataTable').DataTable();
+        $('#dataTable').DataTable({
+            "language": {
+                "lengthMenu": "Mostrando _MENU_ registros por página",
+                "zeroRecords": "No se encuentran registros",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                "infoEmpty": "No se encuentran registros",
+                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "search": "Buscar:",
+                "paginate": {
+                    "first": "Primera",
+                    "last": "Última",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+            }
+        });
     });
 </script>
