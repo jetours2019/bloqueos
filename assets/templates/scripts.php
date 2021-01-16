a7312862a72e660e08efb3141ecbe12e80a83b0b
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 10:47:03 -0500

    more fix

--- assets/templates/scripts.php
+++ assets/templates/scripts.php
@@ -1,9 +1,9 @@
   <!-- Bootstrap core JavaScript-->
-  <script src="../assets/js/jquery/jquery.min.js"></script>
-  <script src="../assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $url; ?>/assets/js/jquery/jquery.min.js"></script>
+  <script src="<?php echo $url; ?>/assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="../assets/js/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo $url; ?>/assets/js/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="../assets/js/sb-admin-2.min.js"></script>
+  <script src="<?php echo $url; ?>/assets/js/sb-admin-2.min.js"></script>
