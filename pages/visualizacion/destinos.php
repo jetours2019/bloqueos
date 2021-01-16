de66927d873aa6e555e1a50b36755865b21aa57f
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 10:27:11 -0500

    navbar en destinos

--- pages/visualizacion/destinos.php
+++ pages/visualizacion/destinos.php
@@ -95,18 +95,15 @@ switch ($mes) {
 
 
 
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Control Bloqueos V1.0</span>
-                <img class="img-profile rounded-circle" src="http://charter.aliadostravel.com/img/logo.png">
-              </a>
-              <!-- Dropdown - User Information -->
 
-            </li>
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            <!-- Nav Item - User Information -->
+            <?php include '../assets/templates/navbar.php' ?>
 
 
           </ul>
+
 
         </nav>
         <!-- End of Topbar -->
