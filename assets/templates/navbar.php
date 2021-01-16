81744747822a7b43448d6097f92eb172f9379067
Camilo Cruz <cruz.camilo@correounivalle.edu.co>
2021-01-16 09:38:14 -0500

    icon and images

--- assets/templates/navbar.php
+++ assets/templates/navbar.php
@@ -5,7 +5,7 @@ session_start();
 <li class="nav-item dropdown no-arrow">
     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-gray-600 small">Alia2.0</span>
-        <img style="width: 142px;height: 50px;border-radius: initial !important;" class="img-profile rounded-circle" src="https://aliadostravel.com/wp-content/uploads/2020/12/Aliados.png">
+        <img style="width: 142px;height: 50px;border-radius: initial !important;" class="img-profile rounded-circle" src="../images/Aliados.png">
     </a>
     <!-- Dropdown - User Information -->
 
